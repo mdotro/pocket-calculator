3854cc24198eaec9105f06d61c8919817b8f55bc
Michael Dotro <mdotro@ucvts.tec.nj.us>
2018-12-20 10:46:06 -0500

    first day

---
+++ calculator.js
@@ -0,0 +1,39 @@
+function one() {
+
+}
+
+function two() {
+
+}
+
+function three() {
+
+}
+
+function four() {
+
+}
+
+function five() {
+
+}
+
+function six() {
+
+}
+
+function seven() {
+
+}
+
+function eight() {
+
+}
+
+function nine() {
+
+}
+
+function zero() {
+
+}
